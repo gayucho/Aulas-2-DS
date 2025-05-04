fe749c786f035dd97c770afa5258bbf982185725
gayucho <lucasbarbosa81612@gmail.comm>
2025-05-03 21:46:22 -0300

    teste

--- Aulas-2-DS/IF_numero_maior.py
+++
@@ -1,9 +0,0 @@
-numero1 = int (input("Insira o primeiro número para comparação: "))
-numero2 = int (input("Insira o segundo número para comparação: "))
-if numero1 > numero2:
-    print("O primeiro é maior")
-elif numero2 > numero1:
-    print("O segundo é maior")
-elif numero1 == numero2:
-    print("Os dois números são iguais")
-    
--- Aulas-2-DS/Vogais.py
+++
@@ -1,4 +0,0 @@
-frase = (input("Digite uma frase: "))
-for letra in frase:
-    if letra in "aeiouAEIOU":
-        print(letra, end=" ")
